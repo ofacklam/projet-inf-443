925001e1d4cfc76e28ca926e2b6b92daf05ef5e4
ofacklam <oliver.facklam@gmail.com>
2019-05-22 10:08:08 +0200

    better trees

--- src/projet/arbre.cpp
+++ src/projet/arbre.cpp
@@ -7,10 +7,23 @@
 
 using namespace vcl;
 
+#define WIDTH_DEC 0.707
+#define BR_LEN_DEC 0.8
+#define TR_LEN_DEC 0.9
+
+enum branch_type {
+    A,
+    B, 
+    C
+};
+
 struct element {
-    bool branche;
-    float theta;
-    float phi;
+    branch_type type;
+    mat3 orientation;
+
+    float start_width;
+    float end_width;
+    float len;
 
     std::vector<int> fils;
 };
@@ -21,6 +34,7 @@ void generateTree(std::vector<element> &tree, int num_iter) {
     std::uniform_real_distribution<float> dist_theta(0, 3.14/2);
     std::uniform_real_distribution<float> dist_phi(0, 2*3.14);
     std::uniform_real_distribution<float> binary(0, 1);
+    std::uniform_real_distribution<float> roll(-3.14/3, 3.14/3);
 
     std::random_device rd;
     std::default_random_engine gen(rd());
@@ -35,62 +49,78 @@ void generateTree(std::vector<element> &tree, int num_iter) {
 
             element elem = tree[idx];
 
-            // Rule no 1 : branch grows
-            if(elem.branche) {
-                if(binary(gen) < 0.3) {
-                    element child_branch;
-                    child_branch.branche = true;
-                    child_branch.phi = 0;
-                    child_branch.theta = 0;
-                    child_branch.fils = elem.fils;
+            float a0 = 3.14/4;
+            float a2 = 3.14/4;
+            float d = 137.5 * 3.14 / 180;
+            float b = 0; //roll(gen);
 
-                    tree.push_back(child_branch);
+            switch(elem.type) {
+                case A: {
+                    element child1;
+                    child1.type = B;
+                    child1.start_width = elem.end_width;
+                    child1.end_width = child1.start_width * WIDTH_DEC;
+                    child1.len = elem.len * BR_LEN_DEC;
+                    child1.orientation = rotation_from_axis_angle_mat3({1, 0, 0}, a0);
+                    elem.fils.push_back(tree.size());
+                    tree.push_back(child1);
 
-                    elem.fils.clear();
-                    elem.fils.push_back(tree.size() - 1);
+                    element child2;
+                    child2.type = A;
+                    child2.start_width = elem.end_width;
+                    child2.end_width = child2.start_width * WIDTH_DEC;
+                    child2.len = elem.len * TR_LEN_DEC;
+                    child2.orientation = rotation_from_axis_angle_mat3({0, 0, 1}, d);
+                    elem.fils.push_back(tree.size());
+                    tree.push_back(child2);
+                    
+                    break;
                 }
-                else {
-                    element child_branch;
-                    child_branch.branche = false;
-                    child_branch.phi = dist_phi(gen);
-                    child_branch.theta = dist_lim_theta(gen);
+                case B: {
+                    element child1;
+                    child1.type = C;
+                    child1.start_width = elem.end_width;
+                    child1.end_width = child1.start_width * WIDTH_DEC;
+                    child1.len = elem.len * BR_LEN_DEC;
+                    child1.orientation = rotation_from_axis_angle_mat3({0, 1, 0}, -a2);
+                    elem.fils.push_back(tree.size());
+                    tree.push_back(child1);
 
-                    tree.push_back(child_branch);
-                    elem.fils.push_back(tree.size() - 1);
+                    element child2;
+                    child2.type = C;
+                    child2.start_width = elem.end_width;
+                    child2.end_width = child2.start_width * WIDTH_DEC;
+                    child2.len = elem.len * TR_LEN_DEC;
+                    child2.orientation = rotation_from_axis_angle_mat3({0, 0, 1}, b);
+                    elem.fils.push_back(tree.size());
+                    tree.push_back(child2);
 
-                    child_branch.phi += 3.14;
-
-                    tree.push_back(child_branch);
-                    elem.fils.push_back(tree.size() - 1);
+                    break;
                 }
-            }
-            else { // Rule no 2 : leaf becomes branch
-                elem.branche = true;
+                case C: {
+                    element child1;
+                    child1.type = B;
+                    child1.start_width = elem.end_width;
+                    child1.end_width = child1.start_width * WIDTH_DEC;
+                    child1.len = elem.len * BR_LEN_DEC;
+                    child1.orientation = rotation_from_axis_angle_mat3({0, 1, 0}, a2);
+                    elem.fils.push_back(tree.size());
+                    tree.push_back(child1);
 
-                // Create straigth leaf
-                /*element straight;
-                straight.branche = false;
-                straight.phi = straight.theta = 0;
-                tree.push_back(straight);
-                elem.fils.push_back(tree.size() - 1);*/
+                    element child2;
+                    child2.type = B;
+                    child2.start_width = elem.end_width;
+                    child2.end_width = child2.start_width * WIDTH_DEC;
+                    child2.len = elem.len * TR_LEN_DEC;
+                    child2.orientation = rotation_from_axis_angle_mat3({0, 0, 1}, b);
+                    elem.fils.push_back(tree.size());
+                    tree.push_back(child2);
 
-                // Create side leaves
-                for(int i = 0; i < 1; i++) {
-                    element e;
-                    e.branche = false;
-                    e.phi = dist_phi(gen);
-                    e.theta = dist_theta(gen);
-                    tree.push_back(e);
-
-                    elem.fils.push_back(tree.size() - 1);
-
-                    //Symetric
-                    e.phi += 3.14;
-                    tree.push_back(e);
-
-                    elem.fils.push_back(tree.size() - 1);
+                    break;
                 }
-            }
+                default:
+                    break;
+            }            
 
             tree[idx] = elem;
 
@@ -139,17 +169,20 @@ void arbre::setup() {
 
     // Start generation with seed
     element root;
-    root.branche = false;
-    root.theta = root.phi = 0;
+    root.orientation = rotation_from_axis_angle_mat3({0, 0, 1}, 0);
+    root.start_width = cyl_radius;
+    root.end_width = cyl_radius;
+    root.type = A;
+    root.len = cyl_length;
 
     std::vector<element> tree;
     tree.push_back(root);
 
-    generateTree(tree, 4);
+    generateTree(tree, 5);
 
 
     // Start rendering from root
-    mesh cylinder = create_branch(cyl_radius, cyl_radius, cyl_length);
+    mesh cylinder = create_branch(root.start_width, root.end_width, root.len);
     hierarchy.add_element(cylinder, "0", "root");
 
     std::queue<std::pair<int, int> > to_treat;
@@ -162,17 +195,16 @@ void arbre::setup() {
         to_treat.pop();
         element elem = tree[idx];
 
-        mesh cyl = create_branch(cyl_radius/depth, cyl_radius/(depth+1), cyl_length);
+        //mesh cyl = create_branch(cyl_radius/depth, cyl_radius/(depth+1), cyl_length);
         //add children
         for(int c : elem.fils) {
             element child = tree[c];
 
-            hierarchy.add_element(cyl, std::to_string(c), std::to_string(idx), {0, 0, cyl_length},
-                rotation_between_vector_mat3({0, 0, 1}, {
-                    std::sin(child.theta) * std::cos(child.phi),
-                    std::sin(child.theta) * std::sin(child.phi),
-                    std::cos(child.theta)
-                })
+            hierarchy.add_element(create_branch(child.start_width, child.end_width, child.len),
+                                    std::to_string(c),
+                                    std::to_string(idx),
+                                    {0, 0, elem.len},
+                                    child.orientation
             );
 
             to_treat.push(std::pair<int, int> (c, depth+1));
